2eccedd7c9944bbf1c11b69c705eecc12fef5078
Marco Velasquez <mvsocram26@gmail.com>
2023-05-07 15:52:23 -0300

    poker2

---
+++ js/index.js
@@ -0,0 +1,27 @@
+// comentario de linea
+
+/*
+comentario 
+de 
+bloque
+*/
+
+let nombre = prompt ('escribe tu nombre')
+let espacioBlanco = ' '
+
+let usuario = 'Bienvenido' + espacioBlanco + nombre 
+
+console.log (usuario)
+
+// operaciones basicas
+
+let n1 = 500
+let n2 = 750
+let n3 = 890
+
+let resultadoSuma = n1 + n2 - n3
+console.log (resultadoSuma)
+
+
+
+
